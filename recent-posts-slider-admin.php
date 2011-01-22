c3ff88f7ef2da6de824bf7ff1763cc705dd5e633
nehagoel <nehagoel@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-01-22 17:41:36 +0000

    tagging version 0.1
    git-svn-id: https://plugins.svn.wordpress.org/recent-posts-slider/tags/0.1@335863 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ recent-posts-slider-admin.php
@@ -0,0 +1,86 @@
+<?php 
+	if ( $_POST['rps_opt_hidden'] == 'Y' ) {
+		
+		$width = $_POST['rps_width'];
+		if ( is_numeric($width) )
+			update_option('rps_width', $width);
+		else
+			$error[] = "Please enter width in numbers.";
+		
+		$height = $_POST['rps_height'];
+		if ( is_numeric($height) )
+			update_option('rps_height', $height);
+		else
+			$error[] = "Please enter height in numbers.";
+		
+		$post_per_slide = $_POST['rps_post_per_slide'];
+		update_option('rps_post_per_slide', $post_per_slide);
+		
+		$total_posts = $_POST['rps_total_posts'];
+		if ( is_numeric($total_posts) )
+			update_option('rps_total_posts', $total_posts);
+		else
+			$error[] = "Please enter total posts in numbers.";
+		
+		$slider_content = $_POST['rps_slider_content'];
+		update_option('rps_slider_content', $slider_content);
+		
+		if ( $slider_content== 1 )
+			rps_post_img_thumb();
+		?>
+		<?php if( empty($error) ){ ?>
+		<div class="updated"><p><strong><?php _e('Settings saved.', 'wp-rp' ); ?></strong></p></div>
+		<?php }else{ ?>
+		<div class="error"><p><strong><?php 
+			foreach ( $error as $key=>$val ) {
+				_e($val); 
+				echo "<br/>";
+			}
+		?></strong></p></div>
+		<?php }
+	} else {
+		$width = get_option('rps_width');
+		$height = get_option('rps_height');
+		$post_per_slide = get_option('rps_post_per_slide');
+		$total_posts = get_option('rps_total_posts');
+		$slider_content = get_option('rps_slider_content');
+	}
+?>
+
+<div class="wrap">
+<?php    echo "<h2>" . __( 'Recent Posts Slider Options', 'rps_opt' ) . "</h2>"; ?>
+
+<form name="rps_form" method="post" action="<?php echo str_replace( '%7E', '~', $_SERVER['REQUEST_URI']); ?>">
+	<input type="hidden" name="rps_opt_hidden" value="Y">
+	<p>	
+		<?php _e("Width"); ?> 
+		<input type="text" name="rps_width" value="<?php echo $width; ?>" size="20"><?php _e(" ex : 200" ); ?>
+	</p>
+	<p>	
+		<?php _e("Height"); ?> 
+		<input type="text" name="rps_height" value="<?php echo $height; ?>" size="20"><?php _e(" ex : 150" ); ?>
+	</p>
+	<p>	
+		<?php _e("Total Posts"); ?> 
+		<input type="text" name="rps_total_posts" value="<?php echo $total_posts; ?>" size="20">
+	</p>
+	<p>	
+		<?php _e("No. of post to show per slide"); ?> 
+		<select name="rps_post_per_slide">
+			<?php for( $i=1; $i<=10; $i++ ){ ?>
+				<option value="<?php echo $i; ?>" <?php if($post_per_slide==$i){echo 'selected';} ?>><?php echo $i; ?></option>
+			<?php } ?>
+		</select>
+	</p>
+	<p>	
+		<?php _e("Slider content"); ?> 
+		<select name="rps_slider_content">
+			<option value="1" <?php if($slider_content==1){echo 'selected';} ?>>Show Picture &amp; description</option>
+			<option value="2" <?php if($slider_content==2){echo 'selected';} ?>>Show title &amp; description</option>
+		</select>
+	</p>
+	<p class="submit">
+	<input type="submit" name="Submit" class="button-primary" value="<?php esc_attr_e('Save Changes') ?>" />
+	</p>
+</form>
+</div>
